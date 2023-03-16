52af800321d545256bdc518f7965deb0e878120d
Harshavardhan Nara <48521516+naraharsha224@users.noreply.github.com>
2023-03-16 19:47:23 +0530

    Create 3. Unfinished Parts [Tesla SQL Interview Question].sql

---
+++ 3. Unfinished Parts [Tesla SQL Interview Question].sql
@@ -0,0 +1,23 @@
+/*
+Tesla is investigating bottlenecks in their production, and they need your help to extract the relevant data. Write a query that determines which parts have begun the assembly process but are not yet finished.
+
+Assumptions
+
+Table parts_assembly contains all parts in production.
+A part with no finish_date is an unfinished part.
+*/
+
+-- Solution 1 
+
+SELECT DISTINCT part
+FROM parts_assembly
+WHERE finish_date IS NULL
+
+-- Solution 2
+
+SELECT part
+FROM parts_assembly
+WHERE finish_date IS NULL
+GROUP BY part
+
+
