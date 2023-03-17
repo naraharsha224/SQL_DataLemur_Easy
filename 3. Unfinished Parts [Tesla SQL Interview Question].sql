58a95aafa5afb69e7009892c57c1367ec0783c7c
Harshavardhan Nara <48521516+naraharsha224@users.noreply.github.com>
2023-03-17 15:29:16 +0530

    Update 3. Unfinished Parts [Tesla SQL Interview Question].sql

--- 3. Unfinished Parts [Tesla SQL Interview Question].sql
+++ 3. Unfinished Parts [Tesla SQL Interview Question].sql
@@ -1,4 +1,7 @@
 /*
+
+https://datalemur.com/questions/tesla-unfinished-parts
+
 Tesla is investigating bottlenecks in their production, and they need your help to extract the relevant data. Write a query that determines which parts have begun the assembly process but are not yet finished.
 
 Assumptions
